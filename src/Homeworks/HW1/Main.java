426e6df2bd640a24bb3e9be0c73472033b729b7e
Licemero4ka <egor.panteleev01@gmail.com>
2023-09-09 13:49:00 +0300

    create feature branch

--- src/Homeworks/HW1/Main.java
+++ src/Homeworks/HW1/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello and welcome to hell!");
+        System.out.print("Hello from feature branch");
+
+
+
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
