c0485e4050c0006279e64df143f7c73397c1459d
Licemero4ka <egor.panteleev01@gmail.com>
2023-09-09 13:31:48 +0300

    Change code

--- src/Homeworks/HW1/Main.java
+++ src/Homeworks/HW1/Main.java
@@ -1,6 +1,7 @@
 package Homeworks.HW1;
 public class Main {
     public static void main(String[] args) {
+
         System.out.print("Hello and welcome to hell!");
 
         for (int i = 1; i <= 5; i++) {
