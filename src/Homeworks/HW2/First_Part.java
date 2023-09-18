62028072d7520b9a176bbea2c7ff9f50973ea446
Licemero4ka <egor.panteleev01@gmail.com>
2023-09-18 22:37:27 +0300

    Array+String HW2

--- src/Homeworks/HW2/First_Part.java
+++ src/Homeworks/HW2/First_Part.java
@@ -3,8 +3,10 @@ package Homeworks.HW2;
 public class First_Part {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i] * 2;
 
-
+        }
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + ",");
         }
