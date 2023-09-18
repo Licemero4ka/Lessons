6a1a7e2610e327bea635c1fa6c4e8c6be4ee7b9e
Licemero4ka <egor.panteleev01@gmail.com>
2023-09-18 14:45:40 +0300

    HW1

--- src/Homeworks/HW1/Feature.java
+++ src/Homeworks/HW1/Feature.java
@@ -1,4 +1,34 @@
 package Homeworks.HW1;
 
+import java.sql.SQLOutput;
+
 public class Feature {
+    public static void main(String[] args) {
+        //First part
+        String name = "Egor";
+        int height = 186;
+        float weight = 80.5f;
+
+        System.out.println("name=" + name + ", height=" + height + ", weight=" + weight);
+        //Second part
+        int a = 1;
+        int b = 2;
+        int buff = a;
+        a = b;
+        b = buff;
+
+        System.out.println("a = " + a + ", b = " + b);
+        //Third part
+        int c = 5;
+        int d = 3;
+
+        c = c + d;
+        d = c - d;
+        c = c - d;
+
+        System.out.println("c = " + c + ", d = " + d);
+
+    }
+
 }
+
