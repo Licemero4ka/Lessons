b1a7a317d41a77a0eebe1c65ba9d92da62174bba
Licemero4ka <egor.panteleev01@gmail.com>
2023-10-18 19:50:08 +0300

    CW10

---
+++ src/CW10/PersonNameComparator.java
@@ -0,0 +1,12 @@
+package CW10;
+
+import java.util.Comparator;
+
+public class PersonNameComparator implements Comparator<Person> {
+
+    public int compare(Person a, Person b){
+
+        return a.getName().compareTo(b.getName());
+    }
+}
+
